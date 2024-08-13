5f3a15eab2bd3a35b5f710aac4859b2f3f5e2de4
Nathan Ferdinando <nferdica@outlook.com>
2024-08-12 22:09:50 -0300

    feat: Styled components

---
+++ src/pages/Login/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Title } from "./styled";
+import { Container } from "../../styles/GlobalStyles";
+
+export default function Login() {
+  return (
+    <Container>
+      <Title>Login</Title>
+      <button type="button">Enviar</button>
+    </Container>
+  )
+}
